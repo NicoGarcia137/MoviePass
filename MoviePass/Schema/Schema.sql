09b2db3c2a3331cf64966e00a3ddd46d1b0c0a28
Nicolas Pafundi <48566352+NicolasPafundi@users.noreply.github.com>
2019-10-18 23:57:01 -0300

    Add files via upload

--- MoviePass/Schema/Schema.sql
+++ MoviePass/Schema/Schema.sql
@@ -26,30 +26,30 @@ create table Movies (
      Language varchar (50) not null ,
      Image varchar (200) not null , 
      Genre int,
-     constraint pk_Id_Movie primary key (Id) 
+     constraint pk_MovieId primary key (Id) 
 );
 
-create table Genre 
+create table Genres 
 (
     Id int ,
-    Name varchar (50) not null ,
+    Description varchar (50) not null ,
     constraint pk_Id primary key (Id) 
 );
 
-create table MovieXGenre
+create table MovieXGenres
 (  Id int auto_increment , 
-    Id_Movie int not null,
-    Id_Genre int not null,
-   constraint pk_Id_MovieXGenre primary key (Id),
-   constraint fk_Id_Movie foreign key (Id_Movie) references Movie (Id), 
-   constraint fk_Id foreign key (Id_Genre) references Genre (Id) 
+    MovieId int not null,
+    GenreId int not null,
+   constraint pk_MovieIdXGenre primary key (Id),
+   constraint fk_MovieId foreign key (MovieId) references Movie (Id), 
+   constraint fk_GenreId foreign key (GenreId) references Genre (Id) 
 );
 create table MoviexFunciones 
 (
     Id int not null ,
     Name varchar(60), 
     constraint pk_Id_Name primary key (Name, Id),
-    constraint fk_Id_Movie foreign key (Name) references Movie (Id),
+    constraint fk_MovieId foreign key (Name) references Movie (Id),
     constraint fk_Id foreign key (Id) references Funciones (Id)
 );
 
