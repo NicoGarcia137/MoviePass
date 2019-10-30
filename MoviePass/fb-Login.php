76f3767286855d714b75767c3387f459605cdc6a
Jlafratta <52461452+Jlafratta@users.noreply.github.com>
2019-10-30 18:46:46 -0300

    .

--- MoviePass/fb-Login.php
+++ MoviePass/fb-Login.php
@@ -22,8 +22,8 @@ if(isset($_GET['state'])) {
 $fb = new \Facebook\Facebook([
     'app_id' => '3079459188747276',
     'app_secret' => '74ec50347eb89037ca8a697ac5d8e437',
-    'default_graph_version' => 'v4.0',
-    //'default_access_token' => '{access-token}', // optional
+    'default_graph_version' => 'v5.0',
+    //'default_access_token' => 'EAArwwHcLrAwBANNBZCtkjCxo7qTT8ycKxfcZAGV2RFSHnQRz7s0fCKupkiONwJ5kavNaylKnX0oV3mEv8D6vpExU3YSfsY6YeGDL7rXbfbB1wAhMkogWED2qKoQJZBS72HcfMMUU2lptAsMQNB1zixp4GHzEyVl7qnC2YU9H7XekLJk3E8oYqs0e5hij4Kg87VNaZArDQ4uFtnixxRjplyEtCFwZBBOeGYJKyzxh6JQZDZD', // optional
 ]);
 /*Step 2 Create the url*/
 if(empty($access_token)) {
@@ -32,20 +32,43 @@ if(empty($access_token)) {
 /*Step 3 : Get Access Token*/
 $access_token = $fb->getRedirectLoginHelper()->getAccessToken();
 /*Step 4: Get the graph user*/
-if(isset($access_token)) {
-    try {
-        $response = $fb->get('/me',$access_token);
-        $fb_user = $response->getGraphUser();
 
-
-        echo  $fb_user->getName();
-          var_dump($fb_user);
-        
-    } catch (\Facebook\Exceptions\FacebookResponseException $e) {
-        echo  'Graph returned an error: ' . $e->getMessage();
-    } catch (\Facebook\Exceptions\FacebookSDKException $e) {
-        // When validation fails or other local issues
-        echo 'Facebook SDK returned an error: ' . $e->getMessage();
-    }
+try {
+    // Returns a `FacebookFacebookResponse` object
+    $response = $fb->get(
+      '/me',
+      'EAArwwHcLrAwBAFpUHdMZB0VpdDBmexNtjHf3JpY8os1JjqEZCxKLoWn5pb0SryH4Ptvg5f6PQrftT6QFaHscjpsIrquFRkIRFZCcExBjnhKVIgCCf8MgeMh1MnVTOMIoL4yWJsNaZCdPdYUmAG1ejFz6BbAB5mEy64k114wVyyAAZAVEBZA9VjXDcw4ler0BoZD'
+    );
+} catch(FacebookExceptionsFacebookResponseException $e) {
+echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+} catch(FacebookExceptionsFacebookSDKException $e) {
+echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
 }
+    $graphNode = $response->getGraphNode();
+
+    var_dump($graphNode);
+    var_dump($fb);
+    var_dump($access_token);
+
+// if(isset($access_token)) {
+//     try {
+//     $response = $fb->get('/me',$access_token);
+    
+//         $fb_user = $response->getGraphUser();
+
+//         echo $fb_user->getName();
+//         echo $fb_user->getEmail();
+        
+//         var_dump($fb_user);
+//         var_dump($access_token);
+        
+//     } catch (\Facebook\Exceptions\FacebookResponseException $e) {
+//         echo  'Graph returned an error: ' . $e->getMessage();
+//     } catch (\Facebook\Exceptions\FacebookSDKException $e) {
+//         // When validation fails or other local issues
+//         echo 'Facebook SDK returned an error: ' . $e->getMessage();
+//     }
+// }
 ?>
