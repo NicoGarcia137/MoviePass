56c106b5d98757acb95c4072af864359e9f8857e
Nicolas Pafundi <48566352+NicolasPafundi@users.noreply.github.com>
2019-11-19 23:00:07 -0300

    Update HomeController.php

--- MoviePass/Controllers/HomeController.php
+++ MoviePass/Controllers/HomeController.php
@@ -36,7 +36,7 @@
             $date1=new DateTime();
             $date2=new DateTime();
             $date1->modify('-1 day');
-            $this->ShowPurchasesStats($date1,$date2);
+            $this->ShowPurchasesStats($date1->format('Y-m-d H:m:s'),$date2->('Y-m-d H:m:s'));
         }
 
         private function GetAllMoviesInshows(){
